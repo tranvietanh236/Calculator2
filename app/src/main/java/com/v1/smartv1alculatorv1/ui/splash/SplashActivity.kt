87bde41e972262033919b96fb6df0b3c6b9b1562
Hinh12 <121923645+Hinh12@users.noreply.github.com>
2024-09-23 13:54:18 +0700

    SmartCalculator_history

--- app/src/main/java/com/v1/smartv1alculatorv1/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/v1/smartv1alculatorv1/ui/splash/SplashActivity.kt
@@ -1,14 +1,8 @@
 package com.v1.smartv1alculatorv1.ui.splash
 
 import android.content.Intent
-import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.v1.smartv1alculatorv1.R
 import com.v1.smartv1alculatorv1.base.BaseActivity
 import com.v1.smartv1alculatorv1.base.BaseViewModel
 import com.v1.smartv1alculatorv1.databinding.ActivitySplashBinding
